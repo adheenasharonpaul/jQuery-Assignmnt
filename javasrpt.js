5230ba7409edddc934b512e5a3a04202b95e96f6
adheenasharonpaul <adheenasharonpaul@gmail.com>
2015-06-11 12:31:36 +0530

    modified .js file

--- javasrpt.js
+++ javasrpt.js
@@ -1,11 +1,11 @@
 $(document).ready(function () 
 {
     
-var url = "https://api.github.com/users/mralexgray/repos";
+var url = "http://jsonblob.com/api/5575522be4b0463c77b9b268";
     
 $.get(url, function (data, status, xhr) {
         
-var tab = document.getElementById("mytab");
+var tab = document.getElementById("mytable");
         
 for (var i = 0; i < data.length; i++)
         
@@ -14,9 +14,9 @@ for (var i = 0; i < data.length; i++)
 $('<tr><td width="50"><div align="center"><p name="mytd' + i + '">' + data[i].name + '</p></div></td><td width="70"><div align="center"><p name="mytd' + i + '">' + data[i].full_name + '</p></div></td><td width="50"><div align="center"><p name="mytd' + i + '">' + data[i].size + '</p></div></td><td width="110"><div align="center"><p name="mytd' + i + '">' + data[i].created_at + '</p></div></td><td width="50"><div align="center"><p name="mytd' + i + '">' + data[i].has_issues + '</p></div></td><td width="50"><div align="center"><p name="mytd' + i + '">' + data[i].has_downloads + '</p></div></td><td width="50"><div align="center"><p name="mytd' + i + '">' + data[i].watchers + '</p></div></td><td width="80"><div align="center"><p name="mytd' + i + '">' + data[i].language + '</p></div></td></tr>').appendTo(tab);
         }
         
-$("#loadfl").hide();
+$("#loadimg").hide();
         
-$("#container").show();
+$("#section").show();
    
  });
 
@@ -24,13 +24,13 @@ $("#container").show();
 
 
 
-function hglt(val)
+function combo(val)
 
 {
     
-var tablength = document.getElementById("mytab").rows.length;
+var tableLength = document.getElementById("mytable").rows.length;
     
-for (var i = 0; i < tablength - 1; i++)
+for (var i = 0; i < tableLength - 1; i++)
     
 {
         
@@ -43,7 +43,7 @@ document.getElementsByName("mytd" + i)[t].style.color = "black";
     
 }
     
-for (var i = 0; i < tablength - 1; i++)
+for (var i = 0; i < tableLength - 1; i++)
     
 {
         
@@ -69,9 +69,9 @@ document.getElementsByName("mytd" + i)[t].style.color = "red";
 function clr()
 {
     
-var tablength = document.getElementById("mytab").rows.length;
+var tableLength = document.getElementById("mytable").rows.length;
     
-for (var i = 0; i < tablength - 1; i++)
+for (var i = 0; i < tableLength - 1; i++)
     
 {
         
